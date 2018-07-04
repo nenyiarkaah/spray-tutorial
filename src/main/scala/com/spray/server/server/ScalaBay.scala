4ab76f987aa8982147321b3f1a51dcb10a6fbc6d
narkaah <nenyi.arkaah@gmail.com>
2018-07-04 19:30:50 +0100

    Get Directive that responds with a Json. Also contains helloRoute val as route.

--- src/main/scala/com/spray/server/server/ScalaBay.scala
+++ src/main/scala/com/spray/server/server/ScalaBay.scala
@@ -3,7 +3,7 @@ package com.spray.server.server
 import akka.actor.ActorSystem
 import com.spray.models.{MultiCrystalSilicon, Silicon}
 import spray.http.MediaTypes
-import spray.routing.SimpleRoutingApp
+import spray.routing.{Route, SimpleRoutingApp}
 
 object ScalaBay extends App with SimpleRoutingApp {
   implicit val actorSystem = ActorSystem()
@@ -11,19 +11,28 @@ object ScalaBay extends App with SimpleRoutingApp {
   // type Route = RequestContext => Unit
   // directives
   var plentyOfSilicon = Silicon.silicons
+  def getJson(route: Route): Route = {
+    get {
+      respondWithMediaType(MediaTypes.`application/json`) {
+        route
+      }
+    }
+  }
+
+  lazy val helloRoute = get {
+    path("hello") {
+      complete { "Welcome to Silicon Valley!" }
+      //         directives
+      //        ctx => ctx.complete("Welcome to Silicon Valley!")
+    }
+  }
 
   startServer(interface = "localhost", port = 8080) {
-    get {
-      path("hello") {
-        complete { "Welcome to Silicon Valley!" }
-//         directives
-//        ctx => ctx.complete("Welcome to Silicon Valley!")
-      }
-    } ~
-    get {
+     helloRoute ~
+    getJson {
       path("list" / "all") {
-        respondWithMediaType(MediaTypes.`application/json`) {
-          complete { Silicon.toJson(plentyOfSilicon) }
+        complete {
+          Silicon.toJson(plentyOfSilicon)
         }
       }
     } ~
