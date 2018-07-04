30fbb821b53c04e222b039768b4c5d3728471b0b
narkaah <nenyi.arkaah@gmail.com>
2018-07-04 16:35:20 +0100

    Creating a json response based on case class silicon List. Combination of routes using ~ method.

--- src/main/scala/com/spray/server/server/ScalaBay.scala
+++ src/main/scala/com/spray/server/server/ScalaBay.scala
@@ -1,15 +1,30 @@
 package com.spray.server.server
 
 import akka.actor.ActorSystem
+import com.spray.models.Silicon
+import spray.http.MediaTypes
 import spray.routing.SimpleRoutingApp
 
 object ScalaBay extends App with SimpleRoutingApp {
   implicit val actorSystem = ActorSystem()
 
+  // type Route = RequestContext => Unit
+  // directives
+  val plentyOfSilicon = Silicon.silicons
+
   startServer(interface = "localhost", port = 8080) {
     get {
       path("hello") {
         complete { "Welcome to Silicon Valley!" }
+//         directives
+//        ctx => ctx.complete("Welcome to Silicon Valley!")
+      }
+    } ~
+    get {
+      path("list" / "all") {
+        respondWithMediaType(MediaTypes.`application/json`) {
+          complete { Silicon.toJson(plentyOfSilicon) }
+        }
       }
     }
   }
